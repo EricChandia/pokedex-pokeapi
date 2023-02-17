bab6b9ecb250d218b45795739867a6176a5db8aa
EricChandia <eric.chandia.ti@gmail.com>
2023-02-17 02:22:30 -0300

    feat: added route to Pokemon Info

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/home';
+import PokeInfo from './pages/pokeInfo';
 
 export default function App() {
   return (
     <>
-      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />;
+          <Route path="/pokemon/:id" element={<PokeInfo />} />;
         </Routes>
       </Router>
     </>
