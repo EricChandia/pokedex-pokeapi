0f587c28ce698c1c479b5114e8c5274d0a2bd88d
EricChandia <eric.chandia.ti@gmail.com>
2023-02-17 12:05:59 -0300

    feat: implemented "Pokedex Sensor" in layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 export default function Header() {
-  return <HeaderContainer>Pokedex</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <PokedexSensorCircle>
+        <PokedexSensor> </PokedexSensor>
+      </PokedexSensorCircle>
+      <h1>Pokedex</h1>
+    </HeaderContainer>
+  );
 }
 
 const HeaderContainer = styled.div`
@@ -12,4 +19,62 @@ const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  h1 {
+    font-size: 4vh;
+  }
 `;
+
+const PokedexSensorCircle = styled.div`
+  width: 5.6vh;
+  height: 5.6vh;
+  margin-left: 8.5%;
+  left: 0;
+
+  position: absolute;
+  background-color: white;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PokedexSensor = styled.div`
+  width: 4.6vh;
+  height: 4.6vh;
+  background-color: #226799;
+  border-radius: 100%;
+`;
+
+// const PokedexLights = styled.div`
+//   width: 15%;
+//   height: 20px;
+//   margin-left: 15%;
+//   margin-bottom: 4%;
+//   position: fixed;
+//   left: 0;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+// `;
+
+// const PokedexLight1 = styled.div`
+//   width: 15px;
+//   height: 15px;
+//   background-color: red;
+//   border-radius: 100%;
+// `;
+
+// const PokedexLight2 = styled.div`
+//   width: 15px;
+//   height: 15px;
+//   background-color: yellow;
+//   border-radius: 100%;
+// `;
+
+// const PokedexLight3 = styled.div`
+//   width: 15px;
+//   height: 15px;
+//   background-color: green;
+//   border-radius: 100%;
+// `;
