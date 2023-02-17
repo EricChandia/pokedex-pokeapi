32bf026983e6c4b06812d423b7720ce8a09b7207
EricChandia <eric.chandia.ti@gmail.com>
2023-02-17 02:25:41 -0300

    feat: moved header to Home page only

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import Pokedex from '../../components/Pokedex';
+import Header from '../../components/Header';
 
 export default function Home() {
   return (
     <HomeContainer>
+      <Header />
       <Pokedex />
     </HomeContainer>
   );
@@ -13,6 +15,7 @@ const HomeContainer = styled.div`
   width: 100vw;
 
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
