2382602963c53f15ef45f5b927245d62130fdc26
EricChandia <eric.chandia.ti@gmail.com>
2023-02-17 02:28:18 -0300

    feat: implemented pokemon Info Page

---
+++ src/pages/pokeInfo/index.js
@@ -0,0 +1,229 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function PokeInfo() {
+  // var sound = new Howl({
+  //   src: ['https://pokemoncries.com/cries-old/1.mp3'],
+  //   volume: 0.8
+  // });
+  // sound.play();
+  const location = useLocation();
+  const { state: pokemon } = location;
+  const [pkmDetails, setPkmDetails] = useState([]);
+  const [loading, setLoading] = useState([]);
+  const [pkmFlavorText, setPkmFlavorText] = useState([]);
+
+  console.log(pokemon);
+
+  async function fetchPokemonDetails() {
+    setLoading(true);
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`);
+    console.log('Pokemon Details: ');
+    console.log(data);
+
+    let flavor_text = data.flavor_text_entries[0].flavor_text;
+
+    flavor_text = flavor_text.replace(/(\r\n|\n|\r|\f)/gm, ' ');
+    console.log(flavor_text);
+
+    setPkmDetails(data);
+    setPkmFlavorText(flavor_text);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPokemonDetails();
+  }, []);
+
+  return (
+    <PokemonContainer>
+      <PokemonInfo>
+        <PokemonImageContainer>
+          <PokemonImage
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={pokemon.name}></PokemonImage>
+        </PokemonImageContainer>
+        <PokemonCardDetails>
+          <CardDetailsTop>
+            #{pokemon.id} {pokemon.name}
+          </CardDetailsTop>
+          <CardDetailsMiddle>
+            <CardDetailsMiddleLeft>
+              <div>Type</div>
+              <div>Height</div>
+              <div>Weight</div>
+              <div>Habitat</div>
+            </CardDetailsMiddleLeft>
+            <CardDetailsMiddleRight>
+              <div>
+                {pokemon.types.map((typeInfo, i, { length }) => {
+                  if (i + 1 === length) {
+                    return `${typeInfo.type.name}`;
+                  } else {
+                    return `${typeInfo.type.name} | `;
+                  }
+                })}
+              </div>
+              <div>{pokemon.height}`</div>
+              <div>{pokemon.weight} lbs</div>
+              {loading ? <>...</> : <div>{pkmDetails.habitat.name}</div>}
+            </CardDetailsMiddleRight>
+          </CardDetailsMiddle>
+          {loading ? <div>...</div> : <CardDetailsBottom>{pkmFlavorText}</CardDetailsBottom>}
+        </PokemonCardDetails>
+      </PokemonInfo>
+    </PokemonContainer>
+  );
+}
+
+const PokemonContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 0 10px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #242423;
+`;
+
+const PokemonInfo = styled.div`
+  width: 30%;
+  height: 90vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 500px) {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (min-width: 500px) {
+    width: 80%;
+    height: 100%;
+  }
+
+  @media (min-width: 700px) {
+    width: 60%;
+    height: 100%;
+  }
+
+  @media (min-width: 900px) {
+    width: 45%;
+    height: 100%;
+  }
+
+  @media (min-width: 1300px) {
+    width: 40%;
+    height: 100%;
+  }
+
+  @media (min-width: 1500px) {
+    width: 35%;
+    height: 100%;
+  }
+`;
+
+const PokemonImageContainer = styled.div`
+  width: 80%;
+  height: 40%;
+
+  margin-top: 5vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PokemonImage = styled.img`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  object-fit: contain;
+`;
+
+const PokemonCardDetails = styled.div`
+  width: 100%;
+  height: 60%;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardDetailsTop = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ecebec;
+`;
+
+const CardDetailsMiddle = styled.div`
+  margin-top: 5px;
+  width: 100%;
+  height: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardDetailsMiddleLeft = styled.div`
+  width: 40%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #ecebec;
+
+  div {
+    height: 25%;
+    width: 100%;
+    border-bottom: 3px solid #e0e0e0;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const CardDetailsMiddleRight = styled.div`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+
+  div {
+    height: 25%;
+    width: 100%;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const CardDetailsBottom = styled.div`
+  margin-top: 5px;
+  padding: 10px 10px;
+  height: 30%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ecebec;
+`;
